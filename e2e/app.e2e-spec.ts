630d3297a50668090da2e81848d06724f464b0a1
Jan Valentik <jvalentik@sk.ibm.com>
2017-04-16 11:48:40 +0200

    Fixed E2E tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,9 +9,30 @@ describe('transporter App', () => {
 
   it('should display message saying home works', (done) => {
     page.navigateTo();
-    page.getParagraphText().then((onResolve) => {
-      expect(onResolve).toEqual('home works!');
-      done();
-    });
+    page.getParagraphText()
+        .then((onResolve) => {
+          expect(onResolve).toEqual('home works!');
+          done();
+        });
+  });
+
+  it('should hide and show side menu', (done) => {
+    page.navigateTo();
+    page
+      .isMenuShown()
+      .then((onResolve) => {
+        expect(onResolve).toBeTruthy();
+        return Promise.resolve;
+      })
+      .then(() => {
+        return page.clickMenu();
+      })
+      .then(() => {
+        return page.isMenuShown();
+      })
+      .then((onResolve) => {
+        expect(onResolve).toBeFalsy();
+        done();
+      });
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,4 +1,5 @@
 import { browser, element, by } from 'protractor';
+import { promise } from 'selenium-webdriver';
 
 export class TransporterPage {
   navigateTo() {
@@ -6,6 +7,24 @@ export class TransporterPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root app-sidenav md-sidenav-container div div app-home p')).getText();
+    return element(
+      by.css('app-root app-sidenav md-sidenav-container div div app-home p'))
+      .getText();
+  }
+
+  clickMenu(): promise.Promise<void> {
+    return element(
+      by.css('app-root app-toolbar md-toolbar div md-toolbar-row button'))
+      .click();
+  }
+
+  isMenuShown(): promise.Promise<boolean> {
+    return element(
+      by.css('app-root app-sidenav md-sidenav-container md-sidenav'))
+      .getAttribute('class')
+      .then((css) => {
+        console.log(JSON.stringify(css));
+        return css.split(' ').includes('mat-sidenav-opened');
+       });
   }
 }
